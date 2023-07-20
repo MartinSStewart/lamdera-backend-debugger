a5ab34db25835e597dfe3a3c00db6e0dd48e0890
Martin Stewart <MartinSStewart@gmail.com>
2023-07-20 13:27:25 +0200

    Another fix

--- src/Frontend.elm
+++ src/Frontend.elm
@@ -213,7 +213,7 @@ updateFromBackend msg model =
                                                 }
                                             )
                                             loaded.history
-                                            |> (\a -> Array.slice msgsToDrop historyLength a)
+                                            |> (\a -> Array.slice msgsToDrop (historyLength + 1) a)
                                     , indexOffset = loaded.indexOffset + msgsToDrop
                                     , selected =
                                         if selectedLatest then
@@ -243,7 +243,7 @@ updateFromBackend msg model =
                                                 }
                                             )
                                             loaded.history
-                                            |> (\a -> Array.slice msgsToDrop historyLength a)
+                                            |> (\a -> Array.slice msgsToDrop (historyLength + 1) a)
                                     , indexOffset = loaded.indexOffset + msgsToDrop
                                     , selected =
                                         if loaded.selected == Array.length loaded.history - 1 then
